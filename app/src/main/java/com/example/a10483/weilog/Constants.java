3ca3258064d8a0b53d5dada80c77138642f4caf7
Leothon Wong <langmancaizi@gmail.com>
2017-09-11 22:05:03 +0800

    开始添加网络功

---
+++ app/src/main/java/com/example/a10483/weilog/Constants.java
@@ -0,0 +1,11 @@
+package com.example.a10483.weilog;
+
+/**
+ * Created by 10483 on 2017/9/11.
+ */
+
+public interface Constants {
+    public static final String APP_KEY="172318056";//appkey
+    public static final String REDIRECT_URL="http://sina.com";//回调页
+    public static final String SCOPE="all";
+}
